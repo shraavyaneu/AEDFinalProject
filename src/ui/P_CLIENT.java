a5fbebe0d8816de989045e50521a92e59a5e9649
Sakshi-Shetty <shetty.sak@northeastern.edu>
2022-12-10 18:53:31 -0500

    Client Java Class - 1

---
+++ src/ui/P_CLIENT.java
@@ -0,0 +1,88 @@
+package ui;
+
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Sakshi Shetty
+ */
+public class P_CLIENT {
+    
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String phone;
+    private String email;
+    private String address;
+    
+    public int getId()
+    {
+        return this.id;
+    }
+    
+    public void setId(int ID)
+    {
+        this.id = ID;
+    }
+    
+    public String getFname()
+    {
+        return this.firstName;
+    }
+    
+    public void setFname(String FNAME)
+    {
+       this.firstName = FNAME; 
+    }
+    
+    public String getLname()
+    {
+        return this.lastName;
+    }
+    
+    public void setLname(String LNAME)
+    {
+       this.lastName = LNAME; 
+    }
+    
+    public String getPhone()
+    {
+        return this.phone;
+    }
+    
+    public void setPhone(String PHONE)
+    {
+       this.phone = PHONE; 
+    }
+    
+    public String getEmail()
+    {
+        return this.email;
+    }
+    
+    public void setEmail(String EMAIL)
+    {
+       this.email = EMAIL; 
+    }
+    
+    public String getAddress()
+    {
+        return this.address;
+    }
+    
+    public void setAddress(String ADDRESS)
+    {
+       this.address = ADDRESS; 
+    }
+    
+}
+    
+    
+    
