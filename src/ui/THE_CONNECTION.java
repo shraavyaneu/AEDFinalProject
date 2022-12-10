8ccd96db8bccdcd25b9c0352f356948f7715f893
Jeswani-Lokesh <114031064+Jeswani-Lokesh@users.noreply.github.com>
2022-12-10 17:59:10 -0500

    Add files via upload

---
+++ src/ui/THE_CONNECTION.java
@@ -0,0 +1,55 @@
+package ui;
+
+
+
+
+import com.mysql.cj.jdbc.MysqlDataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Lokesh
+ */
+public class THE_CONNECTION {
+
+
+    private static String servername = "localhost";
+    private static String dbname = "java_rst_db";
+    private static String username = "root";
+    private static Integer portnumber = 3306;
+    private static String password = "";// no password
+    
+    
+    // create a function to get the connection
+    public static Connection getTheConnection()
+    {
+       
+        Connection connection = null;
+        
+        MysqlDataSource datasource = new MysqlDataSource();
+        
+        datasource.setServerName(servername);
+        datasource.setUser(username);
+        datasource.setDatabaseName(dbname);
+        datasource.setPortNumber(portnumber);
+        datasource.setPassword(password);
+        
+        try {
+            connection  = datasource.getConnection();
+        } catch (SQLException ex) {
+            Logger.getLogger(THE_CONNECTION.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return connection;
+    }
+    
+    
+}
+
+
+
+
+
