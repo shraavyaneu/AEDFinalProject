0a5009600efbf9c9ef6b73cc5ed3b2be3a84ceb2
Jeswani-Lokesh <jeswani.l@northeastern.edu>
2022-12-10 06:06:13 -0500

    LOGIN PAGE MAIN BRANCH

--- src/THE_CONNECTION.java
+++ src/THE_CONNECTION.java
@@ -1,8 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package real_estate_app;
+
+
+
 import com.mysql.cj.jdbc.MysqlDataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -14,19 +12,21 @@ import java.util.logging.Logger;
  * @author Lokesh
  */
 public class THE_CONNECTION {
-    
+
+
     private static String servername = "localhost";
     private static String dbname = "java_rst_db";
     private static String username = "root";
     private static Integer portnumber = 3306;
-    private static String password = "";//no pwd
+    private static String password = "";// no password
     
     
-    //function to get connection
-    
+    // create a function to get the connection
     public static Connection getTheConnection()
     {
+       
         Connection connection = null;
+        
         MysqlDataSource datasource = new MysqlDataSource();
         
         datasource.setServerName(servername);
@@ -35,16 +35,19 @@ public class THE_CONNECTION {
         datasource.setPortNumber(portnumber);
         datasource.setPassword(password);
         
-        
-        
         try {
-            connection = datasource.getConnection();
+            connection  = datasource.getConnection();
         } catch (SQLException ex) {
             Logger.getLogger(THE_CONNECTION.class.getName()).log(Level.SEVERE, null, ex);
         }
         
-        
         return connection;
     }
     
+    
 }
+
+
+
+
+
