3024a1908b656b0eaa639411157e972a36493398
Sakshi-Shetty <shetty.sak@northeastern.edu>
2022-12-10 20:06:49 -0500

    Client Java Class - 2

--- src/ui/P_CLIENT.java
+++ src/ui/P_CLIENT.java
@@ -84,4 +84,126 @@ public class P_CLIENT {
        this.address = ADDRESS; 
     }
     
+    public P_CLIENT(){}
+    
+    public P_CLIENT(int ID, String FNAME, String LNAME, String PHONE, String EMAIL, String ADDRESS)
+    {
+        this.id = ID;
+        this.firstName = FNAME;
+        this.lastName = LNAME;
+        this.phone = PHONE;
+        this.email = EMAIL;
+        this.address = ADDRESS;
+    }
+    
+    
+    // create a function to add a new client
+    // first create the client in the database
+    public boolean addNewClient(P_CLIENT client)
+    {
+        PreparedStatement ps;
+        
+        String addQuery = "INSERT INTO `property_client`(`fname`, `lname`, `phone`, `email`, `address`) VALUES (?,?,?,?,?)";
+        
+        try {
+            ps = THE_CONNECTION.getTheConnection().prepareStatement(addQuery);
+            ps.setString(1, client.getFname());
+            ps.setString(2, client.getLname());
+            ps.setString(3, client.getPhone());
+            ps.setString(4, client.getEmail());
+            ps.setString(5, client.getAddress());
+            
+            return (ps.executeUpdate() > 0);
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(P_CLIENT.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        }
+        
+    }
+    
+    
+    // create a function to edit the selected client data
+    public boolean editClientData(P_CLIENT client)
+    {
+        PreparedStatement ps;
+        
+        String editQuery = "UPDATE `property_client` SET `fname`=?,`lname`=?,`phone`=?,`email`=?,`address`=? WHERE `id`=?";
+        
+        try {
+            ps = THE_CONNECTION.getTheConnection().prepareStatement(editQuery);
+            ps.setString(1, client.getFname());
+            ps.setString(2, client.getLname());
+            ps.setString(3, client.getPhone());
+            ps.setString(4, client.getEmail());
+            ps.setString(5, client.getAddress());
+            ps.setInt(6, client.getId());
+            
+            return (ps.executeUpdate() > 0);
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(P_CLIENT.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        }
+    }
+    
+    
+    // create a function to delete the selected client
+    public boolean deleteClient(int clientId)
+    {
+        PreparedStatement ps;
+        
+        String deleteQuery = "DELETE FROM `property_client` WHERE `id`=?";
+        
+        try {
+            ps = THE_CONNECTION.getTheConnection().prepareStatement(deleteQuery);
+            
+            ps.setInt(1, clientId);
+            
+            return (ps.executeUpdate() > 0);
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(P_CLIENT.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        } 
+    }
+    
+    
+    // create a unction to return an arraylist of clients
+    public ArrayList<P_CLIENT> clientsList()
+    {
+        ArrayList<P_CLIENT> list = new ArrayList<>();
+        
+        Statement st;
+        ResultSet rs;
+        
+        String selectQuery = "SELECT * FROM `property_client`";
+        
+        try {
+            
+            st = THE_CONNECTION.getTheConnection().createStatement();
+            rs = st.executeQuery(selectQuery);
+            
+            P_CLIENT client;
+            
+            while (rs.next()) {
+                
+                client = new P_CLIENT(rs.getInt(1),
+                                    rs.getString(2), 
+                                    rs.getString(3),
+                                    rs.getString(4), 
+                                    rs.getString(5), 
+                                    rs.getString(6));
+                
+                list.add(client);
+                
+            }
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(P_CLIENT.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return list;
+    }
+    
 }
